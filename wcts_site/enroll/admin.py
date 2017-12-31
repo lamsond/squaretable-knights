2204e69548a2d6757d7fe44dad6898068eea50fb
lamsond <31638194+lamsond@users.noreply.github.com>
2017-12-30 21:56:17 -0500

    initial student and career db model

---
+++ wcts_site/enroll/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Major, Student
+
+# Register your models here.
+admin.site.register(Major)
+admin.site.register(Student)
---
+++ wcts_site/enroll/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+#career majors
+class Major(models.Model):
+    shop_name = models.CharField(max_length = 50)
+    applicants = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.shop_name
+
+#student
+class Student(models.Model):
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+    middle_init = models.CharField(max_length = 1, blank=True)
+    email = models.EmailField(max_length = 254)
+    choice_one = models.ForeignKey(Major, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.middle_init != '':
+            return self.first_name + " " + self.middle_init + ". " + self.last_name
+        else:
+            return self.first_name + " " + self.last_name
