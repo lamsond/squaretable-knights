bb31f27f97670c006fa971be0ad0e520080df5de
lamsond <31638194+lamsond@users.noreply.github.com>
2018-01-05 09:01:47 -0500

    Add files via upload
    views.py modified to point to apply_static.index instead of sending filler HttpResponse.
    apply.html attempting to load majors through for loop (still not working).
    apply_static.html is hard coded with career majors.

--- wcts_site/enroll/views.py
+++ wcts_site/enroll/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Student
+from .models import Major
 
 def index(request):
     context = {
@@ -15,7 +16,9 @@ def get_id(request):
     return HttpResponse("Enter student ID")
 
 def apply(request):
-    return HttpResponse("Create a new application")
+    #major_list = Major.objects
+    context = {}#{'major_list': major_list,}
+    return render(request, 'enroll/apply_static.html', context)
 
 def check_status(request, student_id):
     return HttpResponse("Check status of application")
